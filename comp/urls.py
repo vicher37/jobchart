442e116af749088af452531ec5f851eb7f76b989
Vicky Qijing Zhang <vickyqijingzhang@gmail.com>
2015-06-08 08:53:54 -0500

    Update urls.py

--- comp/urls.py
+++ comp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
                        url(r'^(?P<company_id>\d+)/review_summary/$', views.review_summaries, name='review_summary'),
                        url(r'^(?P<company_id>\d+)/company/$', views.company, name='company'),
                        url(r'^search_result/$', views.get_name, name='search_result'),
-                       url(r'^about/$', views.about, name='about'),)
+                       url(r'^about/$', views.about, name='about'),
+                       url(r'^review_analytics/$', views.review_analytics, name='review_analytics'),)
